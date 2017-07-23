c4f3d2c7d74e56b3dff8dddf28ff47a6a53fd4c7
joecatarata <joecatarata@gmail.com>
2017-07-23 19:44:42 +0800

    Chapter 23-24
    Added favorites view, revamped detail.html, used POST methods

--- music/views.py
+++ music/views.py
@@ -13,7 +13,7 @@ from .models import Album, Song
     return HttpResponse(html)
 '''
 
-favorite = False
+
 def index(request):
     all_albums = Album.objects.all()
     # template = loader.get_template('music/index.html')
@@ -30,6 +30,18 @@ def detail(request, album_id):
     return render(request, 'music/detail.html', {'album': album})
     # You can also pass the dictionary file as is
 
-def favorite(request):
 
-    return HttpResponse('')
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+        # ^^^^ gets ID of selected song in radio button, detail.html
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()  # stores changes to database
+        return render(request, 'music/detail.html', {'album': album})
