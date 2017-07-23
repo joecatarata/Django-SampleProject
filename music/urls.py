eeaa999ef74c16d1b42298c4c929fc0c07769930
joecatarata <joecatarata@gmail.com>
2017-07-23 18:55:01 +0800

    Up to chapter 19

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from . import views     #i.e. from current directory, import views
+from . import views  # i.e. from current directory, import views
 
-#File contains url following the pattern "/music/*"
+# File contains url following the pattern "/music/*"
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
-    #/music/album_id
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "detail"),
+    # /music/album_id
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse            #http response sends back html/basic webpage
-from django.conf.urls import include
-from django.template import loader
-from .models import Album
+from django.http import HttpResponse  # http response sends back html/basic webpage
+from django.http import Http404
+from .models import Album, Song
 
 # Create your views here.
 '''def index(request):
@@ -13,12 +12,22 @@ from .models import Album
         html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
     return HttpResponse(html)
 '''
+
+
 def index(request):
     all_albums = Album.objects.all()
-    #template = loader.get_template('music/index.html')
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
-    #return HttpResponse(template.render(context, request))
+    # template = loader.get_template('music/index.html')
+    context = {'all_albums': all_albums, }  # Information that this template (index.html) needs in order to work
+    return render(request, 'music/index.html', context)  # this function returns valid HttpResponse
+    # return HttpResponse(template.render(context, request))
+
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details of Album with id:" + str(album_id)+ "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+        # ^^^^^^look for album with id written in url, the one they passed
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    return render(request, 'music/detail.html', {'album': album})
+    # You can also pass the dictionary file as is
